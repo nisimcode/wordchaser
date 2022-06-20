691338b5f266ca6649764c0661543d4ebcaca97d
nisimgit <nisimlevi@ymail.com>
2022-06-20 22:43:16 +0300

    several changes

--- src/misc.js
+++ src/misc.js
@@ -1,11 +1,5 @@
-export const SERVER_URL = "http://127.0.0.1:8000"
-
-export const NOTES_URL = SERVER_URL + "/notes"
-export const VOCABULARY_URL = SERVER_URL + "/vocabulary"
-
 export const maxTries = 3
 export const maxSecs = 15
-export const gameTime = 10
 export const msgColor = {
   clear: "white",
   negative: "red",
